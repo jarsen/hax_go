db3e2b7e9e70a86ed51309c343d010bcba7b3691
Jason Larsen <jason.larsen@gmail.com>
2018-03-19 21:55:32 -0600

    add expression statement to ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/jarsen/hax/token"
+import (
+	"bytes"
+
+	"github.com/jarsen/hax/token"
+)
 
 type (
 	Node interface {
 		TokenLiteral() string
+		String() string
 	}
 
 	Statement interface {
@@ -21,6 +26,8 @@ type (
 		Statements []Statement
 	}
 
+	// STATEMENTS
+
 	LetStatement struct {
 		Token token.Token
 		Name  *Identifier
@@ -31,6 +38,13 @@ type (
 		Token token.Token
 		Value Expression
 	}
+
+	ExpressionStatement struct {
+		Token      token.Token
+		Expression Expression
+	}
+
+	// OTHERS
 
 	Identifier struct {
 		Token token.Token
@@ -46,11 +60,62 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.Value != nil {
+		out.WriteString(rs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
