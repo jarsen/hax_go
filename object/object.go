aa03a65822b89015f961237e8c41a81a1ffb9f97
Jason Larsen <jason.larsen@gmail.com>
2018-04-01 22:38:35 -0600

    add functions and closures and eval

--- object/object.go
+++ object/object.go
@@ -1,8 +1,15 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/jarsen/hax/ast"
+)
 
 const (
+	FUNCTION_OBJ     = "FUNCTION"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	INTEGER_OBJ      = "INTEGER"
@@ -16,6 +23,12 @@ type (
 	Object interface {
 		Type() ObjectType
 		Inspect() string
+	}
+
+	Function struct {
+		Parameters []*ast.Identifier
+		Body       *ast.BlockStatement
+		Env        *Environment
 	}
 
 	ReturnValue struct {
@@ -37,6 +50,25 @@ type (
 	Null struct {
 	}
 )
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
