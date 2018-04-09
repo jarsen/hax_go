169ebd383723f7f962c9b2d10a075091c20f7bcc
Jason Larsen <jason.larsen@gmail.com>
2018-04-09 11:08:46 -0600

    add hash literals

--- ast/ast.go
+++ ast/ast.go
@@ -72,6 +72,11 @@ type (
 		Elements []Expression
 	}
 
+	HashLiteral struct {
+		Token token.Token // the '{' token
+		Pairs map[Expression]Expression
+	}
+
 	IndexExpression struct {
 		Token token.Token // The [ token
 		Left  Expression
@@ -316,6 +321,23 @@ func (ie *IndexExpression) String() string {
 	out.WriteString("[")
 	out.WriteString(ie.Index.String())
 	out.WriteString("])")
+
+	return out.String()
+}
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/jarsen/hax/ast"
@@ -18,6 +19,7 @@ const (
 	NULL_OBJ         = "NULL"
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ         = "HASH"
 )
 
 type (
@@ -56,6 +58,24 @@ type (
 
 	Array struct {
 		Elements []Object
+	}
+
+	Hashable interface {
+		HashKey() HashKey
+	}
+
+	Hash struct {
+		Pairs map[HashKey]HashPair
+	}
+
+	HashPair struct {
+		Key   Object
+		Value Object
+	}
+
+	HashKey struct {
+		Type  ObjectType
+		Value uint64
 	}
 
 	Null struct {
@@ -121,6 +141,46 @@ func (ao *Array) Inspect() string {
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
+
+	return out.String()
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s",
+			pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
