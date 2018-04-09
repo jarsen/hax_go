ffac4ddf471eca75f2695c122c9890ef91210b92
Jason Larsen <jason.larsen@gmail.com>
2018-04-09 11:16:18 -0600

    add puts builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/jarsen/hax/object"
+import (
+	"fmt"
+
+	"github.com/jarsen/hax/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -105,6 +109,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
